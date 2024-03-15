6319536e88bc3b3c0b31496ad24a197a26950f83
ninjahaipler <abdan,syakur140703@gmail.com>
2024-03-15 21:27:43 +0700

    menambahkan modal

--- js/main.js
+++ js/main.js
@@ -1,44 +1,8 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-    const customSelect = document.querySelector('.custom-select');
-    const selectHeader = customSelect.querySelector('.select-header');
-    const optionsContainer = customSelect.querySelector('.options');
-    const selectedOption = customSelect.querySelector('.selected-option');
-    const change = document.querySelector('.change');
-
-    selectHeader.addEventListener('click', function () {
-        customSelect.classList.toggle('open');
-    });
-
-    optionsContainer.addEventListener('click', function (e) {
-        if (e.target.classList.contains('option')) {
-            const value = e.target.dataset.value;
-            selectedOption.textContent = e.target.textContent;
-            customSelect.classList.remove('open');
-        }
-    });
-
-    document.addEventListener('click', function (e) {
-        if (!customSelect.contains(e.target)) {
-            customSelect.classList.remove('open');
-        }
-    });
-    change.addEventListener('click', function () {
-        MicroModal.init();
-        console.log("click");
-        MicroModal.show('modal-id'); // [1]
-    });
-    MicroModal.init({
-        onShow: modal => console.info(`${modal.id} is shown`), // [1]
-        onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-        openTrigger: 'data-custom-open', // [3]
-        closeTrigger: 'data-custom-close', // [4]
-        openClass: 'is-open', // [5]
-        disableScroll: true, // [6]
-        disableFocus: false, // [7]
-        awaitOpenAnimation: false, // [8]
-        awaitCloseAnimation: false, // [9]
-        debugMode: true // [10]
+    MicroModal.init();
+    document.querySelector('.change').addEventListener('click', function() {
+        // Panggil fungsi untuk menampilkan modal
+        MicroModal.show('modal-1');
       });
 });
 
